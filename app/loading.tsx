5ca869f6a7ddc76f1390ddea5d26fc86360ad18f
MMPgenave <mohammadmp14@gmail.com>
2024-04-03 00:42:03 +0330

    add loader component from react-awesome-loaders

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,9 +1,12 @@
+"use client";
 import React from "react";
+// @ts-ignore
+import { BoxesLoader } from "react-awesome-loaders";
 
 const loading = () => {
   return (
-    <div className="flex h-screen w-full items-center justify-center text-xl text-primary-500">
-      درحال آماده سازی...
+    <div className="flex h-screen w-full items-center justify-center">
+      <BoxesLoader boxColor={"#FF7000"} style={{ marginBottom: "20px" }} desktopSize={"128px"} mobileSize={"80px"} />
     </div>
   );
 };
