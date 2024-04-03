480b6f166c03585935ca7a7a9da51bd645098cbf
MMPgenave <mohammadmp14@gmail.com>
2024-04-03 09:36:30 +0330

    rebuilding....

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -14,20 +14,9 @@ import { URLProps } from "@/types";
 
 export default async function QuestionDetailsPage({ params }: URLProps) {
   const result = await getQuestionById({ questionId: params.id });
-  const {
-    author,
-    tags,
-    title,
-    content,
-    createdAt,
-    answers,
-    views,
-    upvotes,
-    _id,
-    downvotes,
-  } = result?.question;
+  const { author, tags, title, content, createdAt, answers, views, upvotes, _id, downvotes } = result?.question;
   const { userId } = auth();
-  const mongoUser = await getUserById({ userId });
+  const mongoUser = await getUserById({ userId: userId! });
 
   let hasUpvoted: boolean = false;
 
@@ -46,20 +35,9 @@ export default async function QuestionDetailsPage({ params }: URLProps) {
     <>
       <div className="flex w-full flex-col justify-start ">
         <div className="flex w-full flex-col-reverse justify-between gap-5 sm:flex-row sm:items-center sm:gap-2 ">
-          <Link
-            href={`/profile/${author.clerkId}`}
-            className="flex items-center justify-start gap-1"
-          >
-            <Image
-              className="rounded-full"
-              src={author.picture}
-              alt={author.name}
-              width={22}
-              height={22}
-            />
-            <div className="paragraph-semibold text-dark300_light700 mt-1">
-              {author.name}
-            </div>
+          <Link href={`/profile/${author.clerkId}`} className="flex items-center justify-start gap-1">
+            <Image className="rounded-full" src={author.picture} alt={author.name} width={22} height={22} />
+            <div className="paragraph-semibold text-dark300_light700 mt-1">{author.name}</div>
           </Link>
           <div className="flex justify-end">
             <Voting
@@ -75,9 +53,7 @@ export default async function QuestionDetailsPage({ params }: URLProps) {
             />
           </div>
         </div>
-        <div className="h2-semibold text-dark200_light900 mt-3.5 w-full text-right">
-          {title}
-        </div>
+        <div className="h2-semibold text-dark200_light900 mt-3.5 w-full text-right">{title}</div>
         <div className="mt-5 flex flex-wrap items-center gap-6">
           <Metric
             imgUrl={"/assets/icons/clock.svg"}
@@ -105,21 +81,13 @@ export default async function QuestionDetailsPage({ params }: URLProps) {
       <ParseHTML data={content} />
       <div className="flex flex-wrap gap-4">
         {tags.map((tag: any) => {
-          return (
-            <Tag text={tag.text} showScore={false} url="/" key={tag._id} />
-          );
+          return <Tag text={tag.text} showScore={false} url="/" key={tag._id} />;
         })}
       </div>
 
-      <AllAnswers
-        questionId={params.id}
-        userId={JSON.stringify(mongoUser._id)}
-      />
+      <AllAnswers questionId={params.id} userId={JSON.stringify(mongoUser._id)} />
 
-      <AnswersToQuestion
-        questionId={params.id}
-        userId={JSON.stringify(mongoUser._id)}
-      />
+      <AnswersToQuestion questionId={params.id} userId={JSON.stringify(mongoUser._id)} />
     </>
   );
 }
