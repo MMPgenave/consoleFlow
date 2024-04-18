d1140726c64cb6c222065c4519d376db6cbd09c9
MMPgenave <mohammadmp14@gmail.com>
2024-04-18 23:30:44 +0330

    continue on fixing bugs

--- components/shared/Pagination/Pagination.tsx
+++ components/shared/Pagination/Pagination.tsx
@@ -29,17 +29,17 @@ const Pagination = ({ pageNumber, isNext }: PaginationPropsType) => {
       <Button
         onClick={() => handleNavigation("prev")}
         disabled={pageNumber === 1}
-        className="light-border-2 flex min-h-[36px] items-center  justify-center gap-2 border dark:bg-slate-700"
+        className="light-border-2 flex min-h-[36px] items-center justify-center  gap-2 border hover:bg-primary-100/80 dark:bg-slate-700"
       >
         <p className="body-medium text-dark200_light800">قبلی</p>
       </Button>
-      <div className="flex items-center justify-center rounded-md bg-primary-500 px-3.5 py-2">
+      <div className="flex items-center  justify-center rounded-md bg-primary-500 px-3.5 py-2">
         <p className="body-semibold text-light-900">{pageNumber}</p>
       </div>
       <Button
         disabled={!isNext}
         onClick={() => handleNavigation("next")}
-        className="light-border-2 flex min-h-[36px] items-center  justify-center gap-2 border dark:bg-slate-700"
+        className="light-border-2 flex min-h-[36px] items-center justify-center  gap-2 border hover:bg-primary-100/80 dark:bg-slate-700"
       >
         <p className="body-medium text-dark200_light800">بعدی</p>
       </Button>
