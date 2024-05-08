194292740403148f4c4b6409fe213ba450700c97
MMPgenave <mohammadmp14@gmail.com>
2024-05-08 13:36:45 +0330

    sss

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
   openGraph: {
     images: "/assets/images/cover.jpg",
   },
-  metadataBase: new URL("https://console-flow.vercel.app/"),
+  metadataBase: new URL("https://console-flow.vercel.app"),
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
